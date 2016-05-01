14bfcfa7b5a8704afaf34dc27b46fa4de0f3b2e0
Anthony Wedding <anthonywedding@MacBook-Pro-2.local>
2016-05-01 20:34:51 +0200

    First upload to GIT

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    let maxAmount = 100
+    var multipleAmount = 0
+    var currentAmount = 0
+    var outcomeAmount = 0
+    
+    //Outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var whatMultipleBy: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var multipleLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    
+    @IBAction func onAddBtnPressed(sender: UIButton) {
+        
+        calculateAmount()
+        multipleLbl.text = "\(currentAmount) + \(multipleAmount) = \(outcomeAmount)"
+        isGameOver()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton) {
+        
+        if whatMultipleBy.text != nil && whatMultipleBy.text != "" {
+            
+            logo.hidden = true
+            whatMultipleBy.hidden = true
+            playBtn.hidden = true
+            
+            multipleLbl.hidden = false
+            addBtn.hidden = false
+            
+            multipleAmount = Int(whatMultipleBy.text!)!
+        }
     }
-
+    
+    
+    //Functions
+    func calculateAmount() {
+        
+        currentAmount += 5
+        outcomeAmount = currentAmount + multipleAmount
+    }
+    
+    func isGameOver() {
+        
+        if outcomeAmount >= maxAmount {
+            restartGame()
+        }
+    }
+    
+    func restartGame() {
+        
+        multipleAmount = 0
+        currentAmount = 0
+        outcomeAmount = 0
+        
+        multipleLbl.text = "Press Add to Add!"
+        
+        logo.hidden = false
+        whatMultipleBy.hidden = false
+        playBtn.hidden = false
+        
+        multipleLbl.hidden = true
+        addBtn.hidden = true
+    }
 
 }
 
